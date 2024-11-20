0ae9e08bcfe90bd08f8fcfb240d83df21069e087
Avadhut <avadhut24jadhav@gmail.com>
2024-11-20 06:57:14 +0530

    cors origin extra forward slash removed

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'https://booking-app-frontend-qwsc.vercel.app/',
+  origin: 'https://booking-app-frontend-qwsc.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
