b8df6386b951dbb9b33873da5c02d6cf8e0ba929
Avadhut <avadhut24jadhav@gmail.com>
2025-01-15 01:50:44 +0530

    11

--- api/index.js
+++ api/index.js
@@ -41,6 +41,15 @@ app.use(cors({
 }));
 
 app.options('*', cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5174'); // Your frontend URL
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+
 
 //////////////////////////////////////////////////Uplaod AWs//////////////////////////////////
 
