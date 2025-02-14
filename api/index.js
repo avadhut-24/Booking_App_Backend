4ccea26da7cde848eaf17d8482fad8db656cc978
Avadhut <avadhut24jadhav@gmail.com>
2025-02-14 12:37:51 +0530

    frontend url updated

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'http://localhost:5174',
+  origin: 'https://unique-pika-f4c100.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allows cookies and credentials
