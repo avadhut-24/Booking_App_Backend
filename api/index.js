a5c7721a2dd8979dc8b2554bd29c478717745f88
Avadhut <avadhut24jadhav@gmail.com>
2025-02-22 15:31:18 +0530

    updated cors origin

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'https://booking-app-frontend-nine.vercel.app/',
+  origin: 'https://booking-app-frontend-nine.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allows cookies and credentials
